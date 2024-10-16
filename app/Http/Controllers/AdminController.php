5d6dcaa958c173a1fc8ca975fa1caccb546483c5
sajadanari <sajaddehghananari@gmail.com>
2024-10-16 16:11:11 +0330

    Create categories method in admin controller for display categories page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Middleware\AuthAdmin;
 use App\Models\Brand;
+use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -97,5 +98,10 @@ class AdminController extends Controller
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with('status', 'Brand has been deleted succesfully!');
+    }
+
+    public function categories(){
+        $categories = Category::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.categories.index', compact('categories'));
     }
 }
