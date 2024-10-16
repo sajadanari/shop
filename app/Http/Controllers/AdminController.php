b65ab3814f8224d3c42c6438406b8a8a64d40d21
sajadanari <sajaddehghananari@gmail.com>
2024-10-16 14:32:49 +0330

    create brand_delete method in admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -89,4 +89,13 @@ class AdminController extends Controller
         });
         $img->save($destPath. '/'. $imageName);
     }
+
+    public function brand_delete($id){
+        $brand = Brand::find($id);
+        if(File::exists(public_path('uploads/brands'). '/' . $brand->image)){
+            File::delete(public_path('uploads/brands'). '/' . $brand->image);
+        }
+        $brand->delete();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been deleted succesfully!');
+    }
 }
