9d5154d811dcf660a91369310787c30714513aed
sajadanari <sajaddehghananari@gmail.com>
2024-10-16 16:33:06 +0330

    edit duplicated index of categories and turn brand->category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -30,7 +30,7 @@
 @section("admin-content")
 
 <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-    <h3>Brands</h3>
+    <h3>Categories</h3>
     <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
         <li>
             <a href="{{ route('admin.index') }}">
@@ -41,7 +41,7 @@
             <i class="icon-chevron-right"></i>
         </li>
         <li>
-            <div class="text-tiny">Brands</div>
+            <div class="text-tiny">Categories</div>
         </li>
     </ul>
 </div>
@@ -59,7 +59,7 @@
                 </div>
             </form>
         </div>
-        <a class="tf-button style-1 w208" href="{{ route('admin.brands.add') }}"><i
+        <a class="tf-button style-1 w208" href="#"><i
                 class="icon-plus"></i>Add new</a>
     </div>
     <div class="wg-table table-all-user">
@@ -78,27 +78,27 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($brands as $brand)
+                    @foreach ($categories as $category)
                     <tr>
-                        <td>{{ $brand->id }}</td>
+                        <td>{{ $category->id }}</td>
                         <td class="pname">
                             <div class="image">
-                                <img src="{{ asset('uploads/brands') }}/{{ $brand->image }}" alt="{{ $brand->name }}" class="image">
+                                <img src="{{ asset('uploads/categories') }}/{{ $category->image }}" alt="{{ $category->name }}" class="image">
                             </div>
                             <div class="name">
-                                <a href="#" class="body-title-2">{{ $brand->name }}</a>
+                                <a href="#" class="body-title-2">{{ $category->name }}</a>
                             </div>
                         </td>
-                        <td>{{ $brand->slug }}</td>
+                        <td>{{ $category->slug }}</td>
                         <td><a href="#" target="_blank">0</a></td>
                         <td>
                             <div class="list-icon-function">
-                                <a href="{{ route('admin.brands.edit', ['id' => $brand->id]) }}">
+                                <a href="#">
                                     <div class="item edit">
                                         <i class="icon-edit-3"></i>
                                     </div>
                                 </a>
-                                <form action="{{ route('admin.brands.delete', ['id'=> $brand->id]) }}" method="POST">
+                                <form action="#" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <div class="item text-danger delete">
@@ -114,7 +114,7 @@
         </div>
         <div class="divider"></div>
         <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-            {{ $brands->links('pagination::bootstrap-5') }}
+            {{ $categories->links('pagination::bootstrap-5') }}
         </div>
     </div>
 </div>
