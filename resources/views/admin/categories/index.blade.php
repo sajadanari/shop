e772b38bfef05dab143fdcd32af5088ba6852923
sajadanari <sajaddehghananari@gmail.com>
2024-10-17 10:27:08 +0330

    Add category_edit route into categories index view

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -93,7 +93,7 @@
                         <td><a href="#" target="_blank">0</a></td>
                         <td>
                             <div class="list-icon-function">
-                                <a href="#">
+                                <a href="{{ route('admin.category.edit', ['id'=> $category->id]) }}">
                                     <div class="item edit">
                                         <i class="icon-edit-3"></i>
                                     </div>
