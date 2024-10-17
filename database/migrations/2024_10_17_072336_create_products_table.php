a2dd9b0e99b66df7bee9b99150bfa4510db1d8ec
sajadanari <sajaddehghananari@gmail.com>
2024-10-17 11:03:19 +0330

    Add columns to product create table migration

--- database/migrations/2024_10_17_072336_create_products_table.php
+++ database/migrations/2024_10_17_072336_create_products_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description');
+            $table->decimal('regular_price');
+            $table->decimal('sale_price')->nullable();
+            $table->string('SKU');
+            $table->enum('stock_status', ['instock', 'outofstock']);
+            $table->boolean('featured')->default(false);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->bigInteger('brand_id')->unsigned()->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
             $table->timestamps();
         });
     }
