ecd76f7b9f1ccab8038ed2c283be21718e90245c
sajadanari <sajaddehghananari@gmail.com>
2024-10-17 10:38:50 +0330

    Create category_delete method in admin controller for deleting category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -168,4 +168,15 @@ class AdminController extends Controller
         return redirect()->route('admin.categories')->with('status', 'Category has been updated succesfully!');
     }
 
+    public function category_delete($id){
+        $category = Category::find($id);
+        if(File::exists(public_path('uploads/categories'). '/' . $category->image)){
+            File::delete(public_path('uploads/categories'). '/' . $category->image);
+        }
+
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status', 'Category has been deleted succesfully!');
+
+    }
+
 }
