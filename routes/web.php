eb479b34114f0d328eefe6f4449d91e839bb0431
sajadanari <sajaddehghananari@gmail.com>
2024-10-17 10:26:01 +0330

    Create route for category_edit view

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::get('admin/categories', [AdminController::class, 'categories'])->name('admin.categories');
     Route::get('admin/category/add', [AdminController::class, 'category_add'])->name('admin.category.add');
     Route::post('admin/category/store', [AdminController::class, 'category_store'])->name('admin.category.store');
+    Route::get('admin/category/edit/{id}', [AdminController::class, 'category_edit'])->name('admin.category.edit');
 });
 
 Route::middleware(['auth'])->group(function(){
