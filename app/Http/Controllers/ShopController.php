416ef7e4e08dff88703cec4f251a9f7735ff9da8
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 16:32:04 +0330

    Create index method in shop controller

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
+        return to_route('shop', compact('products'));
+    }
 }
