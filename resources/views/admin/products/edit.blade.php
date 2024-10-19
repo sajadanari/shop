6c63ae495643a3c18c6b3171cb119b27d5fddeeb
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 15:40:38 +0330

    Change product update form submit btn. add -> update

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -285,7 +285,7 @@
             @enderror
         </div>
         <div class="cols gap10">
-            <button class="tf-button w-full" type="submit">Add product</button>
+            <button class="tf-button w-full" type="submit">Update product</button>
         </div>
     </div>
 </form>
