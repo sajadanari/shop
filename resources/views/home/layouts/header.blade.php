b3c47fe32ed27dd8ca07d934f3d49f85a44cbb56
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 22:20:12 +0330

    add shop.index route to navbar of home layout

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -51,7 +51,7 @@
               <a href="{{ route('home') }}" class="navigation__link">Home</a>
             </li>
             <li class="navigation__item">
-              <a href="shop.html" class="navigation__link">Shop</a>
+              <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
             </li>
             <li class="navigation__item">
               <a href="cart.html" class="navigation__link">Cart</a>
@@ -141,7 +141,7 @@
             <a href="{{ route('home') }}" class="navigation__link">Home</a>
           </li>
           <li class="navigation__item">
-            <a href="shop.html" class="navigation__link">Shop</a>
+            <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
           </li>
           <li class="navigation__item">
             <a href="cart.html" class="navigation__link">Cart</a>
