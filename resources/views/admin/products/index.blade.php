a4e8f00428679f3eb3172b20fc74a67af23d3d6d
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 08:25:05 +0330

    Add product add route to products index page

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -38,7 +38,7 @@
                 </div>
             </form>
         </div>
-        <a class="tf-button style-1 w208" href="add-product.html"><i
+        <a class="tf-button style-1 w208" href="{{ route('admin.product.add') }}"><i
                 class="icon-plus"></i>Add new</a>
     </div>
     <div class="table-responsive">
