9b4f713f80627053bfe5b497dff7b4c7b339a56a
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 08:16:38 +0330

    A little bit of beautifing on form tags

--- resources/views/admin/products/add.blade.php
+++ resources/views/admin/products/add.blade.php
@@ -35,24 +35,19 @@
 <!-- form-add-product -->
 <form class="tf-section-2 form-add-product" method="POST" enctype="multipart/form-data" action="">
     @csrf
-    <input type="hidden" name="_token" value="8LNRTO4LPXHvbK2vgRcXqMeLgqtqNGjzWSNru7Xx"
-        autocomplete="off">
+    <input type="hidden" name="_token" value="8LNRTO4LPXHvbK2vgRcXqMeLgqtqNGjzWSNru7Xx" autocomplete="off">
     <div class="wg-box">
         <fieldset class="name">
             <div class="body-title mb-10">Product name <span class="tf-color-1">*</span>
             </div>
-            <input class="mb-10" type="text" placeholder="Enter product name"
-                name="name" tabindex="0" value="" aria-required="true" required="">
-            <div class="text-tiny">Do not exceed 100 characters when entering the
-                product name.</div>
+            <input class="mb-10" type="text" placeholder="Enter product name" name="name" tabindex="0" value="" aria-required="true" required="">
+            <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
         </fieldset>
 
         <fieldset class="name">
             <div class="body-title mb-10">Slug <span class="tf-color-1">*</span></div>
-            <input class="mb-10" type="text" placeholder="Enter product slug"
-                name="slug" tabindex="0" value="" aria-required="true" required="">
-            <div class="text-tiny">Do not exceed 100 characters when entering the
-                product name.</div>
+            <input class="mb-10" type="text" placeholder="Enter product slug" name="slug" tabindex="0" value="" aria-required="true" required="">
+            <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
         </fieldset>
 
         <div class="gap22 cols">
@@ -83,22 +78,16 @@
         </div>
 
         <fieldset class="shortdescription">
-            <div class="body-title mb-10">Short Description <span
-                    class="tf-color-1">*</span></div>
-            <textarea class="mb-10 ht-150" name="short_description"
-                placeholder="Short Description" tabindex="0" aria-required="true"
-                required=""></textarea>
-            <div class="text-tiny">Do not exceed 100 characters when entering the
-                product name.</div>
+            <div class="body-title mb-10">Short Description <span class="tf-color-1">*</span></div>
+            <textarea class="mb-10 ht-150" name="short_description" placeholder="Short Description" tabindex="0" aria-required="true" required=""></textarea>
+            <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
         </fieldset>
 
         <fieldset class="description">
             <div class="body-title mb-10">Description <span class="tf-color-1">*</span>
             </div>
-            <textarea class="mb-10" name="description" placeholder="Description"
-                tabindex="0" aria-required="true" required=""></textarea>
-            <div class="text-tiny">Do not exceed 100 characters when entering the
-                product name.</div>
+            <textarea class="mb-10" name="description" placeholder="Description" tabindex="0" aria-required="true" required=""></textarea>
+            <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
         </fieldset>
     </div>
     <div class="wg-box">
@@ -107,16 +96,14 @@
             </div>
             <div class="upload-image flex-grow">
                 <div class="item" id="imgpreview" style="display:none">
-                    <img src="../../../localhost_8000/images/upload/upload-1.png"
-                        class="effect8" alt="">
+                    <img src="../../../localhost_8000/images/upload/upload-1.png" class="effect8" alt="">
                 </div>
                 <div id="upload-file" class="item up-load">
                     <label class="uploadfile" for="myFile">
                         <span class="icon">
                             <i class="icon-upload-cloud"></i>
                         </span>
-                        <span class="body-text">Drop your images here or select <span
-                                class="tf-color">click to browse</span></span>
+                        <span class="body-text">Drop your images here or select <span class="tf-color">click to browse</span></span>
                         <input type="file" id="myFile" name="image" accept="image/*">
                     </label>
                 </div>
@@ -126,18 +113,18 @@
         <fieldset>
             <div class="body-title mb-10">Upload Gallery Images</div>
             <div class="upload-image mb-16">
-                <!-- <div class="item">
-<img src="images/upload/upload-1.png" alt="">
-</div>                                                 -->
+                    <!--
+                    <div class="item">
+                    <img src="images/upload/upload-1.png" alt="">
+                    </div>
+                    -->                                                 
                 <div id="galUpload" class="item up-load">
                     <label class="uploadfile" for="gFile">
                         <span class="icon">
                             <i class="icon-upload-cloud"></i>
                         </span>
-                        <span class="text-tiny">Drop your images here or select <span
-                                class="tf-color">click to browse</span></span>
-                        <input type="file" id="gFile" name="images[]" accept="image/*"
-                            multiple="">
+                        <span class="text-tiny">Drop your images here or select <span class="tf-color">click to browse</span></span>
+                        <input type="file" id="gFile" name="images[]" accept="image/*" multiple="">
                     </label>
                 </div>
             </div>
@@ -147,16 +134,11 @@
             <fieldset class="name">
                 <div class="body-title mb-10">Regular Price <span
                         class="tf-color-1">*</span></div>
-                <input class="mb-10" type="text" placeholder="Enter regular price"
-                    name="regular_price" tabindex="0" value="" aria-required="true"
-                    required="">
+                <input class="mb-10" type="text" placeholder="Enter regular price" name="regular_price" tabindex="0" value="" aria-required="true" required="">
             </fieldset>
             <fieldset class="name">
-                <div class="body-title mb-10">Sale Price <span
-                        class="tf-color-1">*</span></div>
-                <input class="mb-10" type="text" placeholder="Enter sale price"
-                    name="sale_price" tabindex="0" value="" aria-required="true"
-                    required="">
+                <div class="body-title mb-10">Sale Price <span class="tf-color-1">*</span></div>
+                <input class="mb-10" type="text" placeholder="Enter sale price" name="sale_price" tabindex="0" value="" aria-required="true" required="">
             </fieldset>
         </div>
 
@@ -165,15 +147,12 @@
             <fieldset class="name">
                 <div class="body-title mb-10">SKU <span class="tf-color-1">*</span>
                 </div>
-                <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU"
-                    tabindex="0" value="" aria-required="true" required="">
+                <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU" tabindex="0" value="" aria-required="true" required="">
             </fieldset>
             <fieldset class="name">
                 <div class="body-title mb-10">Quantity <span class="tf-color-1">*</span>
                 </div>
-                <input class="mb-10" type="text" placeholder="Enter quantity"
-                    name="quantity" tabindex="0" value="" aria-required="true"
-                    required="">
+                <input class="mb-10" type="text" placeholder="Enter quantity" name="quantity" tabindex="0" value="" aria-required="true" required="">
             </fieldset>
         </div>
 
