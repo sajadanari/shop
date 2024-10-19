f4d31998627a6780a624b162c6939f6d83ba9948
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 14:34:44 +0330

    Create js for showin image and making slug in add product page

--- resources/views/admin/products/add.blade.php
+++ resources/views/admin/products/add.blade.php
@@ -4,6 +4,39 @@
 @endpush
 
 @push("admin-scripts")
+
+<script>
+    $(function(){
+        $('#myFile').on('change', function(e){
+            const photoInp = $('#myFile');
+            const [file] = this.files;
+            if(file){
+                $('#imgpreview img').attr('src', URL.createObjectURL(file));
+                $('#imgpreview').show();
+            }
+        });
+
+        $('#gFile').on('change', function(e){
+            const photoInp = $('#gFile');
+            const gphotos = this.files;
+            $.each(gphotos, function(key, val){
+                $('#galUpload').prepend(`<div class="item gitems"><img src="${URL.createObjectURL(val)}" /></div>`);
+            });
+        });
+
+        $('input[name="name"]').on('change', function(){
+            $('input[name="slug"]').val(StringToSlug($(this).val()));
+        });
+
+    });
+
+    function StringToSlug(Text){
+        return Text.toLowerCase()
+        .replace(/[^\w ]+/g, "")
+        .replace(/ +/g, "-");
+    }
+</script>
+
 @endpush
 
 @section("admin-content")
