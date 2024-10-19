56df7b134032ee7380296bbce37161a81006e1a1
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 17:06:34 +0330

    Create shop index page route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
@@ -10,6 +11,10 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::controller(ShopController::class)->prefix('shop')->name('shop.')->group(function(){
+    Route::get('/', 'index')->name('index');
+});
 
 Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::get('admin', [AdminController::class, 'index'])->name('admin.index');
