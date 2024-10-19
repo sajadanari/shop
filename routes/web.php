1c76a4e83f69c0a4a6208cdab79ceb87631c861e
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 15:59:16 +0330

    Create route for delete product

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::post('admin/product/store', [AdminController::class, 'product_store'])->name('admin.product.store');
     Route::get('admin/product/edit/{id}', [AdminController::class, 'product_edit'])->name('admin.product.edit');
     Route::put('admin/product/update', [AdminController::class, 'product_update'])->name('admin.product.update');
+    Route::delete('admin/product/{id}/delete', [AdminController::class, 'product_delete'])->name('admin.product.delete');
 
 });
 
