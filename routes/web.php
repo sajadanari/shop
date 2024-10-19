4b3517261dada4dd9d5edd72dc0e821fde7ef07e
sajadanari <sajaddehghananari@gmail.com>
2024-10-19 14:39:43 +0330

    Create route for product edit page

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::get('admin/products', [AdminController::class, 'products'])->name('admin.products');
     Route::get('admin/product/add', [AdminController::class, 'product_add'])->name('admin.product.add');
     Route::post('admin/product/store', [AdminController::class, 'product_store'])->name('admin.product.store');
+    Route::get('admin/product/edit/{id}', [AdminController::class, 'product_edit'])->name('admin.product.edit');
 
 });
 
