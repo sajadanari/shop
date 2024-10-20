abc0534694a9544a1c366c3c69270fd05e75fb58
sajadanari <sajaddehghananari@gmail.com>
2024-10-20 15:47:27 +0330

    Add home route to product details page

--- resources/views/home/shop/product_details.blade.php
+++ resources/views/home/shop/product_details.blade.php
@@ -81,7 +81,7 @@
         <div class="col-lg-5">
           <div class="d-flex justify-content-between mb-4 pb-md-2">
             <div class="breadcrumb mb-0 d-none d-md-block flex-grow-1">
-              <a href="#" class="menu-link menu-link_us-s text-uppercase fw-medium">Home</a>
+              <a href="{{ route('home') }}" class="menu-link menu-link_us-s text-uppercase fw-medium">Home</a>
               <span class="breadcrumb-separator menu-link fw-medium ps-1 pe-1">/</span>
               <a href="#" class="menu-link menu-link_us-s text-uppercase fw-medium">The Shop</a>
             </div><!-- /.breadcrumb -->
