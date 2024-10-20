820ea35131ef9c441de44dd4825775e996c154ff
sajadanari <sajaddehghananari@gmail.com>
2024-10-20 15:50:58 +0330

    Add product details route to shop index page

--- resources/views/home/shop/index.blade.php
+++ resources/views/home/shop/index.blade.php
@@ -370,13 +370,13 @@
                             <div class="swiper-container background-img js-swiper-slider" data-settings='{"resizeObserver": true}'>
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide">
-                                        <a href="details.html">
+                                        <a href="{{ route('shop.product.details', ['prod_slug' => $product->slug]) }}">
                                             <img loading="lazy" src="{{ asset('uploads/products'). '/'. $product->image }}" width="330" height="400" alt="{{ $product->name }}" class="pc__img" />
                                         </a>
                                     </div>
                                     @foreach (explode(',', $product->images) as $galleryImg)
                                       <div class="swiper-slide">
-                                            <a href="details.html">
+                                            <a href="{{ route('shop.product.details', ['prod_slug' => $product->slug]) }}">
                                                 <img loading="lazy" src="{{ asset('uploads/products'). '/'. trim($galleryImg) }}" width="330" height="400" alt="{{ $product->name }}" class="pc__img" />
                                             </a>
                                       </div>
@@ -396,7 +396,7 @@
 
                         <div class="pc__info position-relative">
                             <p class="pc__category">{{ $product->category->name }}</p>
-                            <h6 class="pc__title"><a href="details.html">{{ $product->name }}</a></h6>
+                            <h6 class="pc__title"><a href="{{ route('shop.product.details', ['prod_slug' => $product->slug]) }}">{{ $product->name }}</a></h6>
                             <div class="product-card__price d-flex">
                             <span class="money price">
                             @if ($product->sale_price)
