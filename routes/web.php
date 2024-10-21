f7f65f25fbfbadd0eea58f72866238c7ad200d08
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 14:53:31 +0330

    Create route for clear cart items

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::controller(CartController::class)->prefix('cart')->name('cart.')->group(f
     Route::put('/increase-quantity/{rowId}', 'increase_cart_quantity')->name('qty.increase');
     Route::put('/decrease-quantity/{rowId}', 'decrease_cart_quantity')->name('qty.decrease');
     Route::delete('/remove/{rowId}', 'remove_item')->name('item.remove');
+    Route::delete('/clear', 'empty_cart')->name('empty');
 });
 
 Route::middleware(['auth', AuthAdmin::class])->group(function(){
