347d1339e72d15fee4905916a08eef8ed05a6f40
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 10:33:31 +0330

    Add cart index route to home layout header

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -54,7 +54,7 @@
               <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
             </li>
             <li class="navigation__item">
-              <a href="cart.html" class="navigation__link">Cart</a>
+              <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
             </li>
             <li class="navigation__item">
               <a href="about.html" class="navigation__link">About</a>
@@ -144,7 +144,7 @@
             <a href="{{ route('shop.index') }}" class="navigation__link">Shop</a>
           </li>
           <li class="navigation__item">
-            <a href="cart.html" class="navigation__link">Cart</a>
+            <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
           </li>
           <li class="navigation__item">
             <a href="about.html" class="navigation__link">About</a>
