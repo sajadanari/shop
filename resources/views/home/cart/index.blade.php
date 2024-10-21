2c11e978ebae6455e14a950943feb9276be4a9d9
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 14:46:27 +0330

    Create form for deleteing cart item

--- resources/views/home/cart/index.blade.php
+++ resources/views/home/cart/index.blade.php
@@ -96,12 +96,16 @@
                     <span class="shopping-cart__subtotal">${{ $item->subtotal() }}</span>
                 </td>
                 <td>
-                    <a href="#" class="remove-cart">
-                    <svg width="10" height="10" viewBox="0 0 10 10" fill="#767676" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.259435 8.85506L9.11449 0L10 0.885506L1.14494 9.74056L0.259435 8.85506Z" />
-                        <path d="M0.885506 0.0889838L9.74057 8.94404L8.85506 9.82955L0 0.97449L0.885506 0.0889838Z" />
-                    </svg>
+                  <form action="{{ route('cart.item.remove', ['rowId' => $item->rowId]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <a href="javascript:void(0)" class="remove-cart">
+                      <svg width="10" height="10" viewBox="0 0 10 10" fill="#767676" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0.259435 8.85506L9.11449 0L10 0.885506L1.14494 9.74056L0.259435 8.85506Z" />
+                          <path d="M0.885506 0.0889838L9.74057 8.94404L8.85506 9.82955L0 0.97449L0.885506 0.0889838Z" />
+                      </svg>
                     </a>
+                  </form>
                 </td>
                 </tr>    
             @endforeach
