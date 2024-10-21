6b38ec02993404f3f4f340c09c95b0c5e2a7d264
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 10:25:41 +0330

    change links to void in cart index

--- resources/views/home/cart/index.blade.php
+++ resources/views/home/cart/index.blade.php
@@ -7,21 +7,21 @@
     <section class="shop-checkout container">
       <h2 class="page-title">Cart</h2>
       <div class="checkout-steps">
-        <a href="cart.html" class="checkout-steps__item active">
+        <a href="javascript:void(0)" class="checkout-steps__item active">
           <span class="checkout-steps__item-number">01</span>
           <span class="checkout-steps__item-title">
             <span>Shopping Bag</span>
             <em>Manage Your Items List</em>
           </span>
         </a>
-        <a href="checkout.html" class="checkout-steps__item">
+        <a href="javascript:void(0)" class="checkout-steps__item">
           <span class="checkout-steps__item-number">02</span>
           <span class="checkout-steps__item-title">
             <span>Shipping and Checkout</span>
             <em>Checkout Your Items List</em>
           </span>
         </a>
-        <a href="order-confirmation.html" class="checkout-steps__item">
+        <a href="javascript:void(0)" class="checkout-steps__item">
           <span class="checkout-steps__item-number">03</span>
           <span class="checkout-steps__item-title">
             <span>Confirmation</span>
