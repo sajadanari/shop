9cdad49661ddb8b29877253b7157ae3dfc8ed11c
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 11:46:31 +0330

    Modify product details page to add add_to_cart form into related products

--- resources/views/home/shop/product_details.blade.php
+++ resources/views/home/shop/product_details.blade.php
@@ -417,9 +417,19 @@
                   @endforeach
                   
                 </a>
-                <button
-                  class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside"
-                  data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                @if (Cart::instance('cart')->content()->where('id', $related_product->id)->count() > 0)
+                  <a href="{{ route('cart.index') }}" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium btn-warning">Go to Cart</a>
+                @else
+                <form name="addtocart-form" method="post" action="{{ route('cart.add') }}">
+                  @csrf
+                  <input type="hidden" name="id" value="{{ $related_product->id }}" />
+                  <input type="hidden" name="name" value="{{ $related_product->name }}" />
+                  <input type="hidden" name="quantity" value="1" />
+                  <input type="hidden" name="price" value="{{ $related_product->sale_price == '' ? $related_product->regular_price : $related_product->sale_price }}" />
+                  <button type="submit" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium" data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                  
+                </form>
+                @endif
               </div>
 
               <div class="pc__info position-relative">
