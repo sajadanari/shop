5a08f273d759358724a9b0ac0e260436b2fd1200
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 08:43:08 +0330

    Create index method for cart controller to show cart index page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class CartController extends Controller
 {
-    //
+    public function index(){
+        $items = Cart::instance('cart')->content();
+        return view('home.cart.index');
+    }
 }
