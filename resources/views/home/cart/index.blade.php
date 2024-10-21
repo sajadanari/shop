0e65098d25f76871c988047c5fca677cafa9ef2e
sajadanari <sajaddehghananari@gmail.com>
2024-10-21 11:22:31 +0330

    Modify cart item image src to display image correctly

--- resources/views/home/cart/index.blade.php
+++ resources/views/home/cart/index.blade.php
@@ -48,7 +48,7 @@
                 <tr>
                 <td>
                     <div class="shopping-cart__product-item">
-                    <img loading="lazy" src="{{ asset('uploads/products/thumbnails'). '/'. $item->image }}" width="120" height="120" alt="{{ $item->name }}" />
+                    <img loading="lazy" src="{{ asset('uploads/products/thumbnails'). '/'. $item->model->image }}" width="120" height="120" alt="{{ $item->name }}" />
                     </div>
                 </td>
                 <td>
